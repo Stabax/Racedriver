46c830feb716f1e34b27c4b4a9eed1923006e7ea
Stalker2106 <contact@maximemartens.fr>
2016-11-02 13:26:37 -0700

    Updated Console, now input is working, but scrolling is fucked up

--- racedriver/include/Console.hpp
+++ racedriver/include/Console.hpp
@@ -5,8 +5,8 @@
 #include <string>
 #include <vector>
 
-#define FONTSIZE	14
-#define FONTFILE	"./data/font/console.ttf"
+#define FONTSIZE	10
+#define FONTFILE	"./Data/font/console.ttf"
 
 typedef struct s_engine t_engine;
 
@@ -26,6 +26,9 @@ public:
   void clear();
   void writeLine(std::string str);
   void positionOutput();
+
+  //Input
+  void exec(std::string cmd);
   void grabInput(sf::Event &event);
 
   //Logic
@@ -35,10 +38,12 @@ public:
 private:
   bool _open;
   int _margin;
+  unsigned int _height;
   sf::Font _font;
   std::vector<sf::RectangleShape> _geometry;
   std::vector<std::string> _history;
   std::vector<sf::Text> _output;
+  sf::Text _inputText;
   std::string _input;
 };
 
--- racedriver/src/Console.cpp
+++ racedriver/src/Console.cpp
@@ -1,21 +1,25 @@
+#include <iostream>
 #include "Game.hh"
 
 Console::Console(sf::RenderWindow &win)
 {
   int width = win.getSize().x;
-  int height = win.getSize().y / 3;
+  _height = win.getSize().y / 3;
   _margin = 7;
   
   _open = false;
   _font.loadFromFile(FONTFILE);
-  _geometry.push_back(sf::RectangleShape(sf::Vector2f(width, height))); //Frame
+  _inputText.setFont(_font);
+  _inputText.setCharacterSize(10);
+  _inputText.setPosition(10, _height - 20);
+  _geometry.push_back(sf::RectangleShape(sf::Vector2f(width, _height))); //Frame
   _geometry[0].setPosition(0, 0);
   _geometry[0].setFillColor(sf::Color(0, 51, 51, 200));
-  _geometry.push_back(sf::RectangleShape(sf::Vector2f(width - 10, height - 45))); //Content
+  _geometry.push_back(sf::RectangleShape(sf::Vector2f(width - 10, _height - 45))); //Content
   _geometry[1].setPosition(5, 5);
   _geometry[1].setFillColor(sf::Color(0, 102, 102, 180));
   _geometry.push_back(sf::RectangleShape(sf::Vector2f(width - 10, 30))); //Input box
-  _geometry[2].setPosition(5, height - 35);
+  _geometry[2].setPosition(5, _height - 35);
   _geometry[2].setFillColor(sf::Color(0, 102, 102, 180));
 }
 
@@ -47,16 +51,35 @@ void Console::writeLine(std::string str)
 
 void Console::positionOutput()
 {
-  _output.back().setPosition(sf::Vector2f(_margin, _margin + (FONTSIZE * _output.size())));
+  if (_margin + (FONTSIZE * _output.size()) < (_height - 45))
+    _output.back().setPosition(sf::Vector2f(_margin, _margin + (FONTSIZE * _output.size())));
+}
+
+void Console::exec(std::string cmd)
+{
+  if (cmd == "exit")
+    toggle();
 }
 
 void Console::grabInput(sf::Event &event)
 {
+  if (event.key.code >= sf::Keyboard::A && event.key.code <= sf::Keyboard::Z)
+    _input += static_cast<char>(event.key.code + 97);
+  else if (event.key.code == sf::Keyboard::BackSpace && _input.size() > 0)
+    _input.pop_back();
+  else if (event.key.code == sf::Keyboard::Return)
+    {
+      _history.push_back(_input);
+      writeLine(_input);
+      exec(_input);
+      _input.clear();
+    }
 }
 
 bool Console::update(sf::Event &event)
 {
   grabInput(event);
+  _inputText.setString(_input);
   return (true);
 }
 
@@ -68,4 +91,5 @@ void Console::draw(t_engine &engine)
     engine.win.draw(_geometry[i]);
   for (size_t i = 0; i < _output.size(); i++)
     engine.win.draw(_output[i]);
+  engine.win.draw(_inputText);
 }

5ee881b3eab1e8b9198946dbb8237b4424e51ccc
Maxime MARTENS <contact@maximemartens.fr>
2018-10-06 17:44:43 +0200

    applied gitignore

--- Obsolete/old_driver/curl/tests/libtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projets/racedriver/old_driver/curl/tests/libtest
-# Build directory: D:/Projets/racedriver/old_driver/curl/tests/libtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
